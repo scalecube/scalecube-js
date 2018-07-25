1a963f57d691a02522381e3d5350fe90a53355f8
dmitriyslaym <dmitriyslaym@gmail.com>
2018-07-25 19:28:14 +0300

    Implementer RSocketProvider class

--- src/scalecube-transport/provider/Rsocket.js
+++ src/scalecube-transport/provider/Rsocket.js
@@ -1,44 +1,67 @@
 import RSocketWebSocketClient from 'rsocket-websocket-client';
 import WebSocket from 'ws';
-import {JsonSerializers, RSocketClient} from "rsocket-core";
+import { JsonSerializers, RSocketClient } from 'rsocket-core';
+import { Observable } from 'rxjs';
 
 export class RSocketProvider {
   constructor() {
-    this.client;
-  }
-
-  buildClient() {
     this.client = new RSocketClient({
-      // send/receive objects instead of strings/buffers
       serializers: JsonSerializers,
       setup: {
-        // ms btw sending keepalive to server
         keepAlive: 60000,
-        // ms timeout if no keepalive response
         lifetime: 180000,
-        // format of `data`
         dataMimeType: 'application/json',
-        // format of `metadata`
         metadataMimeType: 'application/json',
       },
-      transport: new RSocketWebSocketClient({url: 'ws://localhost:8080', wsCreator: url => new WebSocket(url)}),
+      transport: new RSocketWebSocketClient({ url: 'ws://localhost:8080', wsCreator: url => new WebSocket(url) }),
     });
+    this.cancelConnection = null;
+    this.socket = null;
   }
 
   connect() {
     return new Promise((resolve, reject) => {
-      if (!this.client) {
-        reject('No client has been built.');
-      }
-      let cancel;
-
       this.client.connect().subscribe({
-        onComplete: (socket) => resolve({ socket, cancel }),
+        onComplete: (socket) => {
+          this.socket = socket;
+          resolve();
+        },
         onError: reject,
-        onSubscribe: cancelFn => { cancel = cancelFn }
+        onSubscribe: cancel => { this.cancelConnection = cancel }
       });
     });
   }
+
+  closeConnection() {
+    return new Promise((resolve, reject) => {
+      if (!this.cancelConnection) {
+        return reject('The connection is not opened');
+      }
+      this.cancelConnection();
+      resolve();
+    });
+  }
+
+  request({ type, serviceName, actionName, data }) {
+    const isSingle = type === 'requestResponse';
+    const isStream = type === 'requestStream' || type === 'requestChannel';
+
+    return Observable.create((subscriber) => {
+      let cancelSubscription;
+      this.socket[type]({ data, metadata: { q: `/${serviceName}/${actionName}` }})
+        .subscribe({
+          onComplete: (response) => {
+            isSingle && subscriber.next(response.data);
+            subscriber.complete();
+          },
+          onError: subscriber.error,
+          onSubscribe: cancelFlowable => { cancelSubscription = cancelFlowable }
+        });
+
+      return cancelSubscription;
+    });
+  }
+
 }
 
 
--- test/scalecube-transport/Rsocket.test.js
+++ test/scalecube-transport/Rsocket.test.js
@@ -3,32 +3,17 @@ import { RSocketProvider } from 'src/scalecube-transport/provider/Rsocket';
 describe('Rsocket tests', () => {
   it('Test', async (done) => {
     const rSocketProvider = new RSocketProvider();
-    rSocketProvider.buildClient();
-    const { socket, cancel } = await rSocketProvider.connect();
-    const flowablePojoOne = socket.requestResponse({
-      data: {
-        text: 'Some text to be tested'
-      },
-      metadata: {
-        q: '/greeting/pojo/one'
-      },
+    await rSocketProvider.connect();
+    const stream = rSocketProvider.request({
+      type: 'requestResponse',
+      serviceName: 'greeting',
+      actionName: 'pojo/one',
+      data: { text: 'Some text to be tested' }
+    });
+    stream.subscribe((data) => {
+      console.log('data', data);
+      done();
     });
 
-    flowablePojoOne.subscribe({
-      onComplete: (data) => {
-        expect(data).toEqual({
-          data: {
-            text: 'Echo:Some text to be tested'
-          },
-          metadata: {
-            q: '/greeting/pojo/one'
-          },
-        });
-        console.log('data onComplete', data);
-        done();
-      },
-      onError: error => console.error(error),
-      onSubscribe: cancelFlowable => {  }
-    });
   })
 });
