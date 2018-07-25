e3eedeb3bec7f9cf7f866bd97d9ce22038da21ee
dmitriyslaym <dmitriyslaym@gmail.com>
2018-07-25 17:24:15 +0300

    Connected to rSocket gateway in test

--- src/scalecube-transport/provider/Rsocket.js
+++ src/scalecube-transport/provider/Rsocket.js
@@ -1,19 +1,71 @@
 import RSocketWebSocketClient from 'rsocket-websocket-client';
 import WebSocket from 'ws';
+import {JsonSerializers, RSocketClient} from "rsocket-core";
 
-export const createClient = () => {
-  const client = new RSocketWebSocketClient({
-    url: 'wss://echo.websocket.org',
-    wsCreator: url => new WebSocket(url)
-  });
-  console.log('client', client);
-  // client.send('test');
+class RSocketProvider {
+  constructor() {
+    this.client;
+  }
 
-  client.connectionStatus().subscribe({
-    onNext: onNextData => console.log('onNextData', onNextData),
-    onSubscribe: subscription => subscription.request(Number.MAX_SAFE_INTEGER),
-  });
+  buildClient() {
+    this.client = new RSocketClient({
+      // send/receive objects instead of strings/buffers
+      serializers: JsonSerializers,
+      setup: {
+        // ms btw sending keepalive to server
+        keepAlive: 60000,
+        // ms timeout if no keepalive response
+        lifetime: 180000,
+        // format of `data`
+        dataMimeType: 'application/json',
+        // format of `metadata`
+        metadataMimeType: 'application/json',
+      },
+      transport: new RSocketWebSocketClient({url: 'ws://localhost:8080', wsCreator: url => new WebSocket(url)}),
+    });
+  }
+}
 
-  client.connect();
-};
+const client = new RSocketClient({
+  // send/receive objects instead of strings/buffers
+  serializers: JsonSerializers,
+  setup: {
+    // ms btw sending keepalive to server
+    keepAlive: 60000,
+    // ms timeout if no keepalive response
+    lifetime: 180000,
+    // format of `data`
+    dataMimeType: 'application/json',
+    // format of `metadata`
+    metadataMimeType: 'application/json',
+  },
+  transport: new RSocketWebSocketClient({url: 'ws://localhost:8080', wsCreator: url => new WebSocket(url)}),
+});
+
+// Open the connection
+client.connect().subscribe({
+  onComplete: socket => {
+    const flowable = socket.requestStream({
+      data: {
+        hello: 'world',
+        account: 'yes',
+        text: 'Come on'
+      },
+      metadata: {
+        q: '/greeting/pojo/one'
+      },
+    });
+
+    flowable.subscribe({
+      onComplete: (data) => console.log('done', data),
+      onNext: (data) => console.log('data', data),
+      onError: error => console.error(error),
+      onSubscribe: sub => sub.request(1)
+    });
+
+
+  },
+  onError: error => console.error(error),
+  onSubscribe: cancel => {/* call cancel() to abort */}
+});
 
--- test/scalecube-transport/Rsocket.test.js
+++ test/scalecube-transport/Rsocket.test.js
@@ -1,11 +1,57 @@
 import { createClient } from 'src/scalecube-transport/provider/Rsocket';
+import WebSocket from "ws";
+import RSocketWebSocketClient from "rsocket-websocket-client";
+import {FRAME_TYPES, deserializeFrame, serializeFrame, RSocketClient,
+  JsonSerializers,} from 'rsocket-core';
 
 describe('Rsocket tests', () => {
   it('Test', (done) => {
-    createClient();
+    const client = new RSocketClient({
+      // send/receive objects instead of strings/buffers
+      serializers: JsonSerializers,
+      setup: {
+        // ms btw sending keepalive to server
+        keepAlive: 60000,
+        // ms timeout if no keepalive response
+        lifetime: 180000,
+        // format of `data`
+        dataMimeType: 'application/json',
+        // format of `metadata`
+        metadataMimeType: 'application/json',
+      },
+      transport: new RSocketWebSocketClient({url: 'ws://localhost:8080', wsCreator: url => new WebSocket(url)}),
+    });
+
+    // Open the connection
+    client.connect().subscribe({
+      onComplete: socket => {
+        const flowable = socket.requestStream({
+          data: {
+            hello: 'world',
+            account: 'yes',
+            text: 'Come on'
+          },
+          metadata: {
+            q: '/greeting/pojo/one'
+          },
+        });
+
+        flowable.subscribe({
+          onComplete: (data) => console.log('done', data),
+          onNext: (data) => console.log('data', data),
+          onError: error => console.error(error),
+          onSubscribe: sub => sub.request(1)
+        });
+
+
+      },
+      onError: error => console.error(error),
+      onSubscribe: cancel => {/* call cancel() to abort */}
+    });
 
     setTimeout(() => {
       done();
-    }, 2000)
+    }, 2000);
+
   })
 })
