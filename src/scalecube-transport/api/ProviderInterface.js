1fb42ad4ea67f39a9479fc54ae8fe55c4a7ea2e1
dmitriyslaym <dmitriyslaym@gmail.com>
2018-08-03 13:44:47 +0300

    Added jsdoc comments

--- src/scalecube-transport/api/ProviderInterface.js
+++ src/scalecube-transport/api/ProviderInterface.js
@@ -3,7 +3,24 @@ import { TransportRequest, ProviderConfig } from './types';
 import { Observable } from 'rxjs';
 
 export interface ProviderInterface {
+  /**
+   * Method is used to prepare all the necessary connections to a server using the provided configuration
+   *
+   * @param providerConfig - the configuration, that is used to build the provider
+   * @returns a promise that is resolved with void when all the required connections are established. The promise can be rejected dut to validation errors of the config or due to errors during connection
+   */
   build(providerConfig:ProviderConfig): Promise<void>;
+  /**
+   * Method is used to send a request to a server using the provided previously provider.
+   *
+   * @param transportRequest - headers, data and entrypoint of the request.
+   * @returns an observable, that represents the sequence of data that is extracted from the responses from a server. The observable will emit an error, if no provider was being set on the transport or if connection is lost
+   */
   request(transportRequest:TransportRequest): Observable<any>;
+  /**
+   * Method is used to destroy the provider and close all the active connections.
+   *
+   * @returns a promise that resolves with void, when all the connections are closed. The promise can be rejected, if the provider hasn't been built yet
+   */
   destroy(): Promise<void>;
 }
--- src/scalecube-transport/api/types.js
+++ src/scalecube-transport/api/types.js
@@ -1,15 +1,35 @@
 // @flow
 
 export interface TransportRequest {
+  /**
+   * An object with all the additional options to configure the request (for instance type of request, responses limit)
+   */
   headers: Object;
+  /**
+   * A part of an url that will be added to URI, that is set in provider (starts with "/")
+   */
   entrypoint: string;
+  /**
+   * Custom data, that should be included in the request
+   */
   data: any;
 }
 
 export interface ProviderConfig {
+  /**
+   * The path to a server
+   */
   URI: string;
+  /**
+   * ms btw sending keepAlive to server
+   */
   keepAlive?: number;
-  keepAlive?: number;
+  /**
+   * ms timeout if no keepAlive response
+   */
   lifetime?: number;
+  /**
+   * Custom WebSocket class that will be used in the process of building a provider to create a webSocket instance with the provided URI
+   */
   WebSocket?: any;
 }
