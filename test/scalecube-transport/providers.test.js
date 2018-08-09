26b4be040e57bbccbf197bd1c63d53910a5fdb77
dmitriyslaym <dmitriyslaym@gmail.com>
2018-08-09 14:17:46 +0300

    Added test for multiple requests

--- test/scalecube-transport/providers.test.js
+++ test/scalecube-transport/providers.test.js
@@ -324,78 +324,39 @@ describe('Providers tests', () => {
     );
   });
 
+  it('Creating a few streams for different requests is handled correctly', async (done) => {
+    const transport = await prepareTransport();
+    let updates1 = 0;
+    let isStream1Completed = false;
+    transport.request({ headers: { responsesLimit: 3 }, data: text, entrypoint: '/greeting/many' })
+      .subscribe(
+        (data) => {
+          expect(data).toEqual(getTextResponseMany(updates1)(text));
+          updates1++;
+        },
+        error => console.log('onError 1', error),
+        () => { isStream1Completed = true; }
+      );
+    let updates2 = 0;
+    let isStream2Completed = false;
+    transport.request({ data: text, entrypoint: '/greeting/many' })
+      .subscribe(
+        (data) => {
+          expect(data).toEqual(getTextResponseMany(updates2)(text));
+          updates2++;
+        },
+        error => {},
+        () => { isStream2Completed = true; }
+      );
+
+    setTimeout(() => {
+      expect(updates1).toEqual(3);
+      expect(isStream1Completed).toEqual(true);
+      expect(updates2 > 10).toEqual(true);
+      expect(isStream2Completed).toEqual(false);
+      done();
+    }, 2000);
+
+  });
+
 });
-
-
-// describe('PostMessage tests - OLD', () => {
-//   window.workers = {};
-//   const URI = 'https://localhost:8080';
-//
-//   window.workers[URI] = new Worker(function () {
-//     const Observable = require('rxjs').Observable;
-//     const getTextResponseSingle = text => `Echo:${text}`;
-//     const getTextResponseMany = index => text => `Greeting (${index}) to: ${text}`;
-//     const getFailingOneResponse = text => ({ errorCode: 500, errorMessage: text });
-//     const getFailingManyResponse = text => ({ errorCode: 500, errorMessage: getTextResponseSingle(text) });
-//
-//     self.onmessage = ({ data: { entrypoint, data, requestId } }) => {
-//       if (entrypoint === '/greeting/one') {
-//         postMessage({ requestId, data: getTextResponseSingle(data), completed: false });
-//         postMessage({ requestId, data: undefined, completed: true });
-//       }
-//       if (entrypoint === '/greeting/pojo/one') {
-//         postMessage({ requestId, data: { text: getTextResponseSingle(data) }, completed: false });
-//         postMessage({ requestId, data: undefined, completed: true });
-//       }
-//       if (entrypoint === '/greeting/many') {
-//         Observable.interval(100)
-//           .subscribe(
-//             result => postMessage({ requestId, data: getTextResponseMany(result)(data), completed: false }),
-//             (error) => {},
-//             () => postMessage({ requestId, data: undefined, completed: true })
-//           );
-//       }
-//       if (entrypoint === '/greeting/failing/one') {
-//         postMessage({ requestId, data: getFailingOneResponse(data), completed: false });
-//       }
-//       if (entrypoint === '/greeting/failing/many') {
-//         postMessage({ requestId, data: getTextResponseSingle(data), completed: false });
-//         postMessage({ requestId, data: getTextResponseSingle(data), completed: false });
-//         postMessage({ requestId, data: getFailingManyResponse(data), completed: false });
-//       }
-//     };
-//   });
-//
-//   it('Test webworker-threads', async (done) => {
-//     const transport = new Transport();
-//     await transport.setProvider(PostMessageProvider, { URI });
-//     transport.request({ headers: { responsesLimit: 3 }, data: 'Test for 100', entrypoint: '/greeting/many' })
-//       .subscribe(
-//         data => console.log('onNext', data),
-//         error => console.log('onError', error),
-//         () => console.log('Stream completed!')
-//       );
-//
-//     setTimeout(() => {
-//       transport.removeProvider().then(done);
-//     }, 2000);
-//
-//   });
-//
-//   it('Test webworker-threads 2', async (done) => {
-//     const transport = new Transport();
-//     await transport.setProvider(PostMessageProvider, { URI });
-//     transport.request({ data: 'Test', entrypoint: '/greeting/one' })
-//       .subscribe(
-//         data => console.log('data from transport request', data),
-//         error => console.log('error from transport request', error),
-//         () => console.log('data from transport request COMPLETED!')
-//       );
-//
-//     setTimeout(() => {
-//       transport.removeProvider().then(done);
-//     }, 2000);
-//
-//   });
-//
-// });
