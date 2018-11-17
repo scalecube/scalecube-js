ebf590b0d2df2efa1d78435d44d05709b670dd48
Idan Levin <idan@ilt.co.il>
2018-11-17 12:48:08 +0200

    Refactor to use native proxy

--- packages/scalecube-services/src/services/ProxyContext.js
+++ packages/scalecube-services/src/services/ProxyContext.js
@@ -10,33 +10,36 @@ export class ProxyContext{
     this.router = RoundRobinServiceRouter;
   }
 
+    /**
+     *
+     * @param api
+     *
+     * @param router
+     * @return {api}
+     */
   createProxy(api: any, router: typeof Router){
     const dispatcher = this.microservices.dispatcher().router(router).create();
-    const obj = Object.create(api);
-    Object.getOwnPropertyNames(obj.prototype).map((prop)=>{
-      if( prop !== 'constructor' ) {
-        delete obj[prop];
-      } else {
-        obj[prop] = ()=>{};
-      }
-    });
-    Object.keys(api.meta.methods).map(m=>{
-      obj[m] = (...args)=>{
-        const message = {
-          serviceName: api.name,
-          method: m,
-          data: args
-        };
-        if( api.meta.methods[m].type === 'Promise' ) {
-          return dispatcher.invoke(message);
-        } else if ( api.meta.methods[m].type === 'Observable' ) {
-          return dispatcher.listen(message);
-        } else {
-          return new Error(`service method unknown type error: ${api.name}.${m}`);
+    return new Proxy({}, {
+      get: (target, prop) => {
+        if( api.meta.methods[prop] ) {
+           return (...args) => {
+               const message = {
+                   serviceName: api.name,
+                   method: prop,
+                   data: args
+               };
+               if( api.meta.methods[prop].type === 'Promise' ) {
+                   return dispatcher.invoke(message);
+               } else if ( api.meta.methods[prop].type === 'Observable' ) {
+                   return dispatcher.listen(message);
+               } else {
+                   return Error(`service method unknown type error: ${api.name}.${prop}`);
+               }
+           };
         }
+        return undefined;
       }
     });
-    return obj;
   }
   create(){
     return this.createProxy(this.myapi, this.router);
