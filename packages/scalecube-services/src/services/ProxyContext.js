4deffa5789aead5a7bf69cf9e29369fe39b7079f
Idan Levin <idan@ilt.co.il>
2018-11-25 16:31:56 +0200

    Code cleanup

--- packages/scalecube-services/src/services/ProxyContext.js
+++ packages/scalecube-services/src/services/ProxyContext.js
@@ -7,35 +7,47 @@ import 'rxjs/add/observable/fromPromise';
 import {Router, RoundRobinServiceRouter, Microservices, ServiceDefinition} from ".";
 import type {Message} from ".";
 
+const getHandler = ({dispatcher, meta, mw, microservices}) =>
+    (target, prop) => {
+        if (meta.methods[prop] === undefined) {
+            return undefined;
+        }
+        const type = meta.methods[prop].type;
+        return (...args) => {
+            const message = {
+                serviceName: meta.serviceName,
+                method: prop,
+                data: args
+            };
+            if (meta.methods[prop].type !== "Promise" &&
+                meta.methods[prop].type !== "Observable") {
+                return Error(`service method unknown type error: ${meta.serviceName}.${prop}`);
+            }
+
+            const chain$ = Observable
+                .from([{
+                    message,
+                    serviceDefinition: meta,
+                    thisMs: microservices
+                }])
+                .pipe(source$ => mw(source$))
+                .switchMap(req => type === "Promise" ?
+                    Observable.from(dispatcher.invoke(req.message)) :
+                    dispatcher.listen(req.message)
+                );
+
+            return type === "Promise" ? chain$.toPromise() : chain$;
+        };
+    };
 
 export class ProxyContext {
     myapi: any;
     router: typeof Router;
-    // myMW: (msg:Message, mc: Microservices, def: Object) => Message;
     microservices: Microservices;
 
     constructor(microservices: Microservices) {
         this.microservices = microservices;
         this.router = RoundRobinServiceRouter;
-    }
-
-    call(request) {
-        const mw = this.microservices.mw;
-        //const message = mw(request.message, this.microservices, request.meta);
-
-        const chain$ = Observable
-            .from([{
-                message: request.message,
-                serviceDefinition: request.meta,
-                thisMs: this.microservices
-            }])
-            .pipe(source$ => mw(source$))
-            .switchMap(req => request.type === "Promise" ?
-                Observable.from(request.dispatcher.invoke(req.message)) :
-                request.dispatcher.listen(req.message)
-            );
-
-        return request.type === "Promise" ? chain$.toPromise() : chain$;
     }
 
     /**
@@ -48,6 +60,9 @@ export class ProxyContext {
     createProxy(api: any, router: typeof Router) {
         const dispatcher = this.microservices.dispatcher().router(router).create();
         const meta = api.meta;
+        const mw = this.microservices.mw;
+        const microservices = this.microservices;
+
 
         if (!meta) {
             return Error("API must have meta property");
@@ -61,28 +76,7 @@ export class ProxyContext {
             return Error("meta.methods is not defined");
         }
         return new Proxy({}, {
-            get: (target, prop) => {
-                if (meta.methods[prop]) {
-                    return (...args) => {
-                        const message = {
-                            serviceName: meta.serviceName,
-                            method: prop,
-                            data: args
-                        };
-                        if (meta.methods[prop].type !== "Promise" &&
-                            meta.methods[prop].type !== "Observable") {
-                            return Error(`service method unknown type error: ${meta.serviceName}.${prop}`);
-                        }
-                        return this.call({
-                            message,
-                            dispatcher,
-                            meta,
-                            type: meta.methods[prop].type
-                        });
-                    };
-                }
-                return undefined;
-            }
+            get: getHandler({dispatcher, meta, mw, microservices})
         });
     }
 
