8798e554c4bef816f2242006df2ef0ea5d096148
Igor Voitsekhivskyi <orhey@ukr.net>
2018-12-18 13:48:48 +0200

    init

--- packages/scalecube-services/src/services/index.js
+++ packages/scalecube-services/src/services/index.js
@@ -1,32 +1,19 @@
 // @flow
 
-import { Message } from './Message';
-import { DispatcherContext } from './DispatcherContext';
-import { Microservices } from './Microservices';
-import { ServiceInstance } from './ServiceInstance';
-import { ProxyContext } from './ProxyContext';
-import { RoundRobinServiceRouter } from './RoundRobinRouter';
-import { Router } from './Router';
-import { ServiceCall } from './ServiceCall';
-import { ServicesConfig } from './ServicesConfig';
-import { ServiceDefinition } from './ServiceDefinition';
-import { ServiceRegistery } from './ServiceRegistery';
-import { ServicePromise } from './ServicePromise';
+export { Message } from './Message';
+export { DispatcherContext } from './DispatcherContext';
+export { Microservices } from './Microservices';
+export { ServiceInstance } from './ServiceInstance';
+export { ProxyContext } from './ProxyContext';
+export { RoundRobinServiceRouter } from './RoundRobinRouter';
+export { Router } from './Router';
+export { ServiceCall } from './ServiceCall';
+export { ServicesConfig } from './ServicesConfig';
+export { ServiceDefinition } from './ServiceDefinition';
+export { ServiceRegistery } from './ServiceRegistery';
+export { ServicePromise } from './ServicePromise';
 import * as utils from './utils';
 
 export {
-  Message,
-  DispatcherContext,
-  Microservices,
-  ServiceInstance,
-  ProxyContext,
-  RoundRobinServiceRouter,
-  Router,
-  ServiceCall,
-  ServicesConfig,
-  ServiceDefinition,
-  ServiceRegistery,
-  ServicePromise,
   utils
 };
-
