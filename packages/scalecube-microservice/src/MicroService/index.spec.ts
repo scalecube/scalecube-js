c6b50c504cdcf8a15142bdce1184c4892571f012
idokatz <idok.mail@gmail.com>
2019-02-12 15:52:46 +0200

    scalecube-microservice asDispatcher testing

---
+++ packages/scalecube-microservice/src/MicroService/index.spec.ts
@@ -0,0 +1,93 @@
+import { MicroService } from './index';
+import { getGreetingServiceInstance } from '../../__mocks__/GreetingService';
+import { defaultRouter } from '../Routers/default';
+
+describe('Testing MicroService', () => {
+  const defaultUser = 'defaultUser';
+  let subscriber;
+  beforeEach(() => {
+    subscriber && subscriber.unsubscribe();
+  });
+
+  const ms = MicroService.create({
+    services: [getGreetingServiceInstance()],
+  });
+
+  it('MicroService will have asProxy method defined', () => {
+    expect(ms.asProxy).toBeDefined();
+  });
+
+  it('MicroService will have asDispatcher method defined', () => {
+    expect(ms.asDispatcher).toBeDefined();
+  });
+
+  describe('Creating Proxy from MicroService', () => {
+    const greetingService = ms.asProxy({
+      serviceContract: getGreetingServiceInstance(),
+      router: defaultRouter,
+    });
+
+    it('Invoke method that define in the contract', () => {
+      expect(greetingService.hello(defaultUser)).resolves.toEqual(`Hello ${defaultUser}`);
+    });
+
+    it('Subscribe method that define in the contract', () => {
+      expect.assertions(2);
+      subscriber = greetingService.greet$([`${defaultUser}1`, `${defaultUser}2`]).subscribe((res) => {
+        expect(res).toMatch(defaultUser);
+      });
+    });
+  });
+
+  describe('Creating Dispatcher from MicroService', () => {
+    const greetingDispatcher = ms.asDispatcher({
+      router: defaultRouter,
+    });
+
+    it('Invoke method from dispatcher', () => {
+      const message = {
+        serviceName: 'GreetingService',
+        methodName: 'hello',
+        data: [defaultUser],
+        proxy: null,
+      };
+
+      expect(greetingDispatcher.invoke(message).resolves.toEqual(`Hello ${defaultUser}`));
+    });
+
+    it('Listen to method from dispatcher', () => {
+      expect.assertions(2);
+
+      const message = {
+        serviceName: 'GreetingService',
+        methodName: 'greet$',
+        data: [[`${defaultUser}1`, `${defaultUser}2`]],
+        proxy: null,
+      };
+
+      expect(
+        greetingDispatcher.listen(message).subscribe((res) => {
+          expect(res).toMatch(defaultUser);
+        })
+      );
+    });
+
+    it('dispatcher method not found', (done) => {
+      const message = {
+        serviceName: 'GreetingService',
+        methodName: 'fakeHello',
+        data: [defaultUser],
+        proxy: null,
+      };
+
+      try {
+        greetingDispatcher.invoke(message);
+      } catch (e) {
+        expect(e.message).toEqual(
+          `can't find services with the request: '{"serviceName":"GreetingService","methodName":"fakeHello","data":["defaultUser"],"proxy":null}'`
+        );
+        done();
+      }
+    });
+  });
+});
--- packages/scalecube-microservice/src/Routers/default.ts
+++ packages/scalecube-microservice/src/Routers/default.ts
@@ -5,6 +5,10 @@ import { RouteRequest } from '../api/Routers';
 export const defaultRouter = Object.freeze({
   route: ({ request, serviceRegistry }: RouteRequest) => {
     const services = lookUp({ serviceRegistry, namespace: getServiceNamespaceFromMessage(request) });
-    return services[Object.keys(services)[0]];
+    if (!services) {
+      throw Error(`can't find services with the request: '${JSON.stringify(request)}'`);
+    } else {
+      return services[Object.keys(services)[0]];
+    }
   },
 });
