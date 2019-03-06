85d0217f837151e57eff0176da52163e6f19e2ee
idokatz <idok.mail@gmail.com>
2019-03-06 14:40:23 +0200

    debug

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,7 +9,9 @@ if [[ "$TRAVIS_BRANCH" =~ ^feature\/.*$ ]]; then
     echo "|    Deploying snapshot on npm registry    |"
     echo "--------------------------------------------"
     git remote set-url origin https://${GH_TOKEN}@github.com/scalecube/scalecube-js.git
+    git status
     git checkout $TRAVIS_BRANCH
+    git status
     lerna publish --canary --preid snapshot.$(date +%s) --yes -m '[skip ci]'
     if [[ "$?" == 0 ]]; then
         echo $MSG_PUBLISH_SUCCESS
