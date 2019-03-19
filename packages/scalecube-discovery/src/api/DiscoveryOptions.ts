3749a56cda587dc01efb5c4b0c46ebeddbf6a6ef
idokatz <idok.mail@gmail.com>
2019-03-19 11:19:11 +0200

    update after review: - Put TSDoc comments on API

--- packages/scalecube-discovery/src/api/DiscoveryOptions.ts
+++ packages/scalecube-discovery/src/api/DiscoveryOptions.ts
@@ -1,5 +1,15 @@
 import { Item } from '../helpers/types';
 
+/**
+ * DiscoveryOptions - The parameters that are required to add Node to the Cluster
+ *
+ * @param address - Microservice unique address.
+ *
+ * @param endPoints - Endpoints to share in the Cluster.
+ *
+ * @param seedAddress - address of the Cluster seed.
+ *
+ */
 export default interface DiscoveryOptions {
   address: string;
   endPoints: Item[];
