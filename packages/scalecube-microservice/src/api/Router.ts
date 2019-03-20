deb4b8d1fe0daecce70e832ac82e0bb7e6cbc71c
dmitriyslaym <dmitriyslaym@gmail.com>
2019-03-20 09:52:01 +0200

    Fix doc style

--- packages/scalecube-microservice/src/api/Router.ts
+++ packages/scalecube-microservice/src/api/Router.ts
@@ -6,7 +6,7 @@ import { Endpoint, RouteOptions } from '.';
 export default interface Router {
   /**
    * The method that returns the appropriate remoteService for the provided search criteria. Returns null if no
-   appropriate remoteService was found
+   * appropriate remoteService was found
    */
   route: (routeOptions: RouteOptions) => Endpoint | null;
 }
