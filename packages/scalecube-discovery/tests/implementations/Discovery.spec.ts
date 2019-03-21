ab356f05be62c307714c25aed24d4c748eb0308c
dmitriyslaym <dmitriyslaym@gmail.com>
2019-03-21 18:22:21 +0200

    Work in progress for fixing tests

--- packages/scalecube-discovery/tests/implementations/Discovery.spec.ts
+++ packages/scalecube-discovery/tests/implementations/Discovery.spec.ts
@@ -2,18 +2,19 @@ import { createDiscovery } from '../../src/Discovery/Discovery';
 import { Discovery } from '../../src/api';
 import { expectWithFailNow } from '../helpers/utils';
 
-describe('test', () => {
+describe('Discovery tests', () => {
   beforeEach(() => {
     window.scalecube.clusters = {};
   });
 
-  const createEndpoint = (methodIndex: number = 1, serviceIndex: number = 1) => {
+  const createEndpoint = (methodIndex: number = 1, clusterIndex: number = 1, serviceIndex: number = 1) => {
+    const id = methodIndex + clusterIndex;
     const serviceName = `serviceName${serviceIndex}`;
-    const methodName = `methodName${methodIndex}`;
+    const methodName = `methodName${id}`;
     const transport = 'transport';
     const qualifier = `${serviceName}/${methodName}`;
     const uri = `${transport}/${qualifier}`;
-    const address = `address${methodIndex}`;
+    const address = `address${id}`;
     return {
       uri,
       transport,
@@ -25,14 +26,14 @@ describe('test', () => {
     };
   };
 
-  const createDiscoveriesWithSameSeedAddress = (index: number = 10) => {
-    const seedAddress = `cluster${index}`;
-    const endPoint1 = createEndpoint(index + 1);
-    const endPoint2 = createEndpoint(index + 2);
-    const endPoint3 = createEndpoint(index + 3);
-    const discovery1 = createDiscovery({ address: 'address1', seedAddress, endPoints: [endPoint1] });
-    const discovery2 = createDiscovery({ address: 'address2', seedAddress, endPoints: [endPoint2] });
-    const discovery3 = createDiscovery({ address: 'address3', seedAddress, endPoints: [endPoint3] });
+  const createDiscoveriesWithSameSeedAddress = (clusterIndex: number = 1) => {
+    const seedAddress = `cluster${clusterIndex}`;
+    const endPoint1 = createEndpoint(1, clusterIndex);
+    const endPoint2 = createEndpoint(2, clusterIndex);
+    const endPoint3 = createEndpoint(3, clusterIndex);
+    const discovery1 = createDiscovery({ address: `address1${clusterIndex}`, seedAddress, endPoints: [endPoint1] });
+    const discovery2 = createDiscovery({ address: `address2${clusterIndex}`, seedAddress, endPoints: [endPoint2] });
+    const discovery3 = createDiscovery({ address: `address3${clusterIndex}`, seedAddress, endPoints: [endPoint3] });
     return {
       seedAddress,
       endPoint1,
@@ -65,11 +66,11 @@ describe('test', () => {
     expect(window.scalecube.clusters[seedAddress].allEndPoints).toHaveLength(3);
   });
 
-  const testNodesContent = (index: number = 1) => {
-    const { seedAddress, endPoint1, endPoint2, endPoint3 } = createDiscoveriesWithSameSeedAddress(index);
+  const testNodesContent = (clusterIndex: number = 1) => {
+    const { seedAddress, endPoint1, endPoint2, endPoint3 } = createDiscoveriesWithSameSeedAddress(clusterIndex);
     window.scalecube.clusters[seedAddress].nodes.forEach((node, nodeIndex) => {
       expect(Object.keys(node)).toHaveLength(3);
-      expect(node.address).toBe(`'address${nodeIndex + 1}`);
+      expect(node.address).toBe(`address${nodeIndex + 1}${clusterIndex}`);
       switch (nodeIndex) {
         case 0: {
           expect(node.endPoints).toEqual([endPoint2, endPoint3]);
@@ -95,7 +96,7 @@ describe('test', () => {
     () => {
       expect.assertions(9);
 
-      testNodesContent(1);
+      testNodesContent(10);
     }
   );
 
@@ -121,7 +122,7 @@ describe('test', () => {
       ' multiple cluster)',
     () => {
       expect.assertions(18);
-      [1, 2].forEach(testNodesContent);
+      [10, 20].forEach(testNodesContent);
     }
   );
 
@@ -171,26 +172,41 @@ describe('test', () => {
     }, 1000);
   });
 
-  it('Discovery.destroy will remove itself from the cluster, notify other nodes and complete the notifier', async (done) => {
+  it('Discovery.destroy will remove itself from the cluster', async () => {
+    expect.assertions(6);
+
+    const { seedAddress, endPoint2, endPoint3, discovery1, discovery2, discovery3 } = createDiscoveriesWithSameSeedAddress();
+    await discovery1.destroy();
+    expect(window.scalecube.clusters[seedAddress].nodes).toHaveLength(2);
+    expect(window.scalecube.clusters[seedAddress].allEndPoints).toEqual([endPoint2, endPoint3]);
+    await discovery2.destroy();
+    expect(window.scalecube.clusters[seedAddress].nodes).toHaveLength(1);
+    expect(window.scalecube.clusters[seedAddress].allEndPoints).toEqual([endPoint3]);
+    await discovery3.destroy();
+    expect(window.scalecube.clusters[seedAddress].nodes).toHaveLength(0);
+    expect(window.scalecube.clusters[seedAddress].allEndPoints).toHaveLength(0);
+  });
+
+  it('Discovery.destroy will notify other nodes and complete the notifier', async (done) => {
     expect.assertions(8);
 
-    const seedAddress = 'cluster1';
-    const endPoint1 = createEndpoint(1);
-    const endPoint2 = createEndpoint(2);
-    const endPoint3 = createEndpoint(3);
-    const endPoint4 = createEndpoint(4);
-    const discovery1 = createDiscovery({ address: 'address1', seedAddress, endPoints: [endPoint1] });
-    const discovery2 = createDiscovery({ address: 'address2', seedAddress, endPoints: [endPoint2] });
-    const discovery3 = createDiscovery({ address: 'address3', seedAddress, endPoints: [endPoint3] });
+    const {
+      seedAddress,
+      endPoint1,
+      endPoint2,
+      endPoint3,
+      discovery1,
+      discovery2,
+      discovery3,
+    } = createDiscoveriesWithSameSeedAddress(1);
+    const endPoint4 = createEndpoint(4, 1);
 
     testNotifier(discovery1, [[endPoint2, endPoint3], [endPoint2], [endPoint2, endPoint4]], done);
     testNotifier(discovery2, [[endPoint1, endPoint3], [endPoint1], [endPoint1, endPoint4]], done);
     testNotifier(discovery3, [[endPoint1, endPoint2]], done);
 
     await discovery3.destroy();
-    const discovery4 = createDiscovery({ address: 'address4', seedAddress, endPoints: [endPoint4] });
+    const discovery4 = createDiscovery({ address: 'address41', seedAddress, endPoints: [endPoint4] });
     testNotifier(discovery4, [[endPoint1, endPoint2]], done, true);
-
-    // setTimeout(done, 1000);
   });
 });
