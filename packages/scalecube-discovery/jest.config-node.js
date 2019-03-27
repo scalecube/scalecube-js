3ce990958ed6f2512ef230c4c2bc6c1f1089444d
dmitriyslaym <dmitriyslaym@gmail.com>
2019-03-27 08:48:24 +0200

    Code cleanup

--- packages/scalecube-discovery/jest.config-node.js
+++ packages/scalecube-discovery/jest.config-node.js
@@ -1,6 +1,5 @@
 const jestConfig = require('./jest.config-dom');
 
 jestConfig.testEnvironment = 'node';
-// jestConfig.testPathIgnorePatterns.push('<rootDir>/tests/implementations/ImportHtmlService.spec.ts');
 
 module.exports = jestConfig;
--- packages/scalecube-microservice/src/helpers/utils.ts
+++ packages/scalecube-microservice/src/helpers/utils.ts
@@ -19,7 +19,3 @@ export const throwErrorFromServiceCall = ({
 };
 
 export const getGlobalNamespace = () => (typeof window === 'undefined' ? global : window);
-
-export const getScalecubeGlobal = () => {
-  return getGlobalNamespace().scalecube || {};
-};
