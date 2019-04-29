755d9b42a27741131c001908447d05ebb232a6f4
Roykee <41155894+RoykeeOM2@users.noreply.github.com>
2019-04-29 13:24:30 +0300

    Editted and changed.

--- packages/scalecube-microservice/tests/integration/success-coverage-localCall.spec.ts
+++ packages/scalecube-microservice/tests/integration/success-coverage-localCall.spec.ts
@@ -5,6 +5,7 @@ import GreetingService, {
   greet$,
 } from '../mocks/GreetingService';
 import { Microservices } from '../../src';
+import { Message } from '../../src/api';
 
 describe(`Test LocalCall - a microservice instance use its own services.
           # 1. creating a microservice with service & serviceDefinition.
@@ -34,69 +35,76 @@ describe(`Test LocalCall - a microservice instance use its own services.
 	             | class       | # class without static methods
                | static class| # class not object with static methods
                | Plan Object | # Object format is [key]: function
-        And  serviceDefinition that define the contract of the service
-        And  creating proxy and serviceCall
+        And    serviceDefinition that define the contract of the service
+        And    creating proxy and serviceCall
 		`,
     (service) => {
       const microservice = Microservices.create({
         services: [service],
       });
-      const proxy = microservice.createProxy({ serviceDefinition: greetingServiceDefinition }); //
-      const serviceCall = microservice.createServiceCall({});
 
-      test.each([
-        {
-          eachMethod: proxy.hello,
-          eachData: [defaultUser],
-        },
-        {
-          eachMethod: serviceCall.requestResponse,
-          eachData: {
-            qualifier: `${greetingServiceDefinition.serviceName}/hello`,
-            data: [defaultUser],
+      /*test.each([
+          {
+            eachMethod: proxy.hello,
+            eachData: [defaultUser],
           },
-        },
-      ])(
-        `
-        Given eachMethod of type requestResponse and eachData
-        When invoking requestResponse's method with valid data/message
-        Then successful RequestResponse is receive
-        			`,
-        (info) => {
-          const { method, data } = info;
-          return expect(method(data)).resolves.toEqual(`Hello ${defaultUser}`);
-        }
-      );
-
+          {
+            eachMethod: serviceCall.requestResponse,
+            eachData: {
+              qualifier: `${greetingServiceDefinition.serviceName}/hello`,
+              data: [defaultUser],
+            },
+          },
+        ])*/
       test(`
-        When invoking requestResponse's method with valid data/message
-        Then successful RequestResponse is receive
+        # Testing proxy for a successful response.
+        When  invoking requestResponse's method with valid data
+        Then  successful RequestResponse is received
         			`, () => {
-        const response = proxy.hello(defaultUser);
-        return expect(response).resolves.toEqual(`Hello ${defaultUser}`);
+        const proxy = microservice.createProxy({ serviceDefinition: greetingServiceDefinition });
+        return expect(proxy.hello(defaultUser)).resolves.toEqual(`Hello ${defaultUser}`);
       });
 
-      test.each([
-        [proxy.greet$, [`${defaultUser}`]],
-        [
-          serviceCall.requestStream,
-          {
-            qualifier: `${greetingServiceDefinition.serviceName}/greet$`,
-            data: [[`${defaultUser}`]],
-          },
-        ],
-      ])(
-        `
-        When subscribe to RequestStream's method with valid data/message
-        Then successful RequestStream is emitted
-        			`,
-        (invokeMethod, invokeData, done) => {
-          invokeMethod(invokeData).subscribe((response: string) => {
-            expect(response).toEqual(`greetings ${defaultUser}`);
-            done();
-          });
-        }
-      );
+      test(`
+        # Testing serviceCall for a successful response.
+        When  invoking serviceCall's requestResponse method with valid message
+        Then  successful RequestResponse is received
+        `, () => {
+        const message: Message = {
+          qualifier: `${greetingServiceDefinition.serviceName}/hello`,
+          data: [`${defaultUser}`],
+        };
+        const serviceCall = microservice.createServiceCall({});
+        return expect(serviceCall.requestResponse(message)).resolves.toEqual(`Hello ${defaultUser}`);
+      });
+
+      test(`
+        # Testing serviceCall for a successful subscription (array).
+        When  subscribe to RequestStream's method with valid data/message
+        Then  successful RequestStream is emitted
+        			`, (done) => {
+        const message: Message = {
+          qualifier: `${greetingServiceDefinition.serviceName}/greet$`,
+          data: [[`${defaultUser}`]],
+        };
+        const serviceCall = microservice.createServiceCall({});
+
+        serviceCall.requestStream(message).subscribe((response: any) => {
+          expect(response).toEqual(`greetings ${defaultUser}`);
+          done();
+        });
+      });
+      test(`
+        # Testing serviceCall for a successful subscription (array).
+        When  subscribe to RequestStream's method with valid data/message
+        Then  successful RequestStream is emitted
+        `, (done) => {
+        const proxy = microservice.createProxy({ serviceDefinition: greetingServiceDefinition });
+        proxy.greet$([defaultUser]).subscribe((response: any) => {
+          expect(response).toEqual(`greetings ${defaultUser}`);
+          done();
+        });
+      });
     }
   );
 });
