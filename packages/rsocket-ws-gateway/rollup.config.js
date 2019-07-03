377b5db4dfc4d7f25842eef71fc95cb9aa00154d
Viktor Pyshnenko <vpyshnenko@jivygroup.com>
2019-07-03 14:40:37 +0300

    rename gatewat => rscoket-ws-gateway

--- packages/rsocket-ws-gateway/rollup.config.js
+++ packages/rsocket-ws-gateway/rollup.config.js
@@ -1,27 +1,36 @@
+import babel from 'rollup-plugin-babel';
 import visualizer from 'rollup-plugin-visualizer';
 import typescript from 'rollup-plugin-typescript2';
+import tscompile from 'typescript';
+import filesize from 'rollup-plugin-filesize';
+import commonjs from 'rollup-plugin-commonjs';
 import pkg from './package.json';
 
 export default {
   input: 'src/index.ts',
   output: [
     {
+      name: 'gateway',
       file: pkg.main,
       format: 'cjs',
-    },
-    {
-      file: pkg.module,
-      format: 'es',
     },
   ],
   external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
   plugins: [
-    typescript({
-      typescript: require('typescript'),
-      clean: true,
+    babel({
+      babelrc: false,
+      exclude: 'node_modules/**',
+      runtimeHelpers: true,
     }),
+    commonjs({ include: 'node_modules/**' }),
     visualizer({
       filename: 'report.html',
     }),
+    typescript({
+      typescript: tscompile,
+      clean: true,
+    }),
+
+    filesize(),
   ],
 };
