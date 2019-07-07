6c78ece2a0f9b7574b56bd40f340bcbd877cb793
idokatz <idok.mail@gmail.com>
2019-07-07 11:58:50 +0300

    multiple web worker example

--- packages/examples/src/webWorkerExample/index.js
+++ packages/examples/src/webWorkerExample/index.js
@@ -8,36 +8,18 @@
 
 window.addEventListener('DOMContentLoaded', (event) => {
   (function(Microservices, ASYNC_MODEL_TYPES, definitions) {
-    var placeHolder = document.getElementById('placeHolder');
+    var placeHolder1 = document.getElementById('placeHolder');
     var placeHolder2 = document.getElementById('placeHolder2');
     var waitMessage = document.getElementById('waitMessage');
 
     waitMessage.innerText = 'Wait for service ~ 2s';
 
     var worker = new Worker('worker1.js');
-    // var worker2 = new Worker('worker2.js');
+    var worker2 = new Worker('worker2.js');
 
-    worker.addEventListener('message', (ev) => {
-      ev.workerId = 1;
-      console.log('worker to window: ', ev.data);
-      postMessage(ev.data, '*', ev.ports || undefined);
-    });
-
-    // worker2.addEventListener('message', (ev) => {
-    //   ev.workerId = 2;
-    //   postMessage(ev.data, '*', ev.ports || undefined);
-    // });
-
-    addEventListener('message', (ev) => {
-      if (!ev.workerId) {
-        console.log('window to worker: ', ev.data);
-        worker.postMessage(ev.data, ev.ports || undefined);
-      }
-
-      // if ( ev.workerId !== 2 ) {
-      //   worker2.postMessage(ev.data, ev.ports || undefined);
-      // }
-    });
+    var connect = connectWorkers();
+    connect.addWorker(worker);
+    connect.addWorker(worker2);
 
     var localMS = Microservices.create({ services: [], address: 'main' });
     var proxyConfig = {
@@ -46,10 +28,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
           serviceDefinition: definitions.remoteServiceDefinition,
           proxyName: 'awaitProxyName',
         },
-        // {
-        //   serviceDefinition: definitions.remoteServiceDefinition2,
-        //   proxyName: 'awaitProxyName2',
-        // }
+        {
+          serviceDefinition: definitions.remoteServiceDefinition2,
+          proxyName: 'awaitProxyName2',
+        },
       ],
       isAsync: true,
     };
@@ -62,27 +44,27 @@ window.addEventListener('DOMContentLoaded', (event) => {
         serviceNameProxy
           .bubbleSortTime()
           .then((response) => {
-            createLineHTML({ response, type: ASYNC_MODEL_TYPES.REQUEST_RESPONSE, placeHolder });
+            createLineHTML({ response, type: ASYNC_MODEL_TYPES.REQUEST_RESPONSE, placeHolder: placeHolder1 });
           })
           .catch(console.log);
       }
     });
 
-    // awaitProxyName2.then(({ proxy: serviceNameProxy }) => {
-    //   console.log('remote service2 is available!');
-    //   let i = 0;
-    //   for ( i; i < 5; i++ ) {
-    //     serviceNameProxy
-    //       .bubbleSortTime()
-    //       .then((response) => {
-    //         createLineHTML({ response, type: ASYNC_MODEL_TYPES.REQUEST_RESPONSE, placeHolder: placeHolder2 });
-    //       })
-    //       .catch(console.log);
-    //   }
-    // });
+    awaitProxyName2.then(({ proxy: serviceNameProxy }) => {
+      console.log('remote service2 is available!');
+      let i = 0;
+      for (i; i < 5; i++) {
+        serviceNameProxy
+          .bubbleSortTime()
+          .then((response) => {
+            createLineHTML({ response, type: ASYNC_MODEL_TYPES.REQUEST_RESPONSE, placeHolder: placeHolder2 });
+          })
+          .catch(console.log);
+      }
+    });
 
     function createLineHTML({ response, type, placeHolder }) {
-      waitMessage.innerText = 'Service is available:';
+      waitMessage.innerText = 'Service is available from workers:';
 
       var responseSpan = document.createElement('div');
       responseSpan.innerText = `${type}: start at ${response.start}, end at ${response.end}, time ${response.time}`;
@@ -90,3 +72,34 @@ window.addEventListener('DOMContentLoaded', (event) => {
     }
   })(window.sc.Microservices, window.sc.ASYNC_MODEL_TYPES, definitions);
 });
+
+var connectWorkers = function() {
+  var workersMap = {};
+
+  addEventListener('message', (ev) => {
+    if (ev && ev.data && !ev.data.workerId) {
+      if (ev.data.detail) {
+        // console.log('window to worker: ', ev.data, workersMap);
+        var propogateTo = workersMap[ev.data.detail.to] || workersMap[ev.data.detail.address]; //discoveryEvents || rsocketEvents
+        propogateTo && propogateTo.postMessage(ev.data, ev.ports || undefined);
+      }
+    }
+  });
+
+  return {
+    addWorker: function(worker) {
+      worker.addEventListener('message', (ev) => {
+        if (ev && ev.data && ev.data.type === 'membershipEventInitServer') {
+          workersMap = {
+            ...workersMap,
+            [ev.data.detail.origin]: worker,
+          };
+        }
+
+        ev.data.workerId = 1;
+        // console.log('worker to window: ', ev.data);
+        postMessage(ev.data, '*', ev.ports || undefined);
+      });
+    },
+  };
+};
