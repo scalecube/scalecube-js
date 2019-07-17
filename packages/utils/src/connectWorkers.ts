3b85f70eb5dcf7eded7c2baceab779d6984a64a7
idokatz <idok.mail@gmail.com>
2019-07-17 18:17:33 +0300

    fix ts issue

--- packages/utils/src/connectWorkers.ts
+++ packages/utils/src/connectWorkers.ts
@@ -20,21 +20,25 @@ if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScop
 }
 
 function workerEventHandler(ev: any) {
-  if (!ev.data.workerId) {
-    ev.data.workerId = 1;
+  if (ev.data && ev.data.detail && ev.data.type) {
+    if (!ev.data.workerId) {
+      ev.data.workerId = 1;
 
-    if (ev.data.type === 'ConnectWorkerEvent') {
-      // @ts-ignore
-      workersMap[ev.data.detail.whoAmI] = this;
-    } else {
-      const propogateTo = workersMap[ev.data.detail.to] || workersMap[ev.data.detail.address]; // discoveryEvents || rsocketEvents
-      // console.log('worker -> propogateTo', ev.data);
-      if (propogateTo) {
-        // @ts-ignore
-        propogateTo.postMessage(ev.data, ev.ports || undefined);
+      if (ev.data.type === 'ConnectWorkerEvent') {
+        if (ev.data.detail.whoAmI) {
+          // @ts-ignore
+          workersMap[ev.data.detail.whoAmI] = this;
+        }
       } else {
-        // @ts-ignore
-        postMessage(ev.data, '*', ev.ports || undefined);
+        const propogateTo = workersMap[ev.data.detail.to] || workersMap[ev.data.detail.address]; // discoveryEvents || rsocketEvents
+        // console.log('worker -> propogateTo', ev.data);
+        if (propogateTo) {
+          // @ts-ignore
+          propogateTo.postMessage(ev.data, ev.ports || undefined);
+        } else {
+          // @ts-ignore
+          postMessage(ev.data, '*', ev.ports || undefined);
+        }
       }
     }
   }
