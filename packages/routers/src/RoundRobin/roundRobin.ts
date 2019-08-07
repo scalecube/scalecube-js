e42955cc51e74a6516442baceed08135c70fa10f
idokatz <idok.mail@gmail.com>
2019-08-07 09:44:44 +0300

    router tests

--- packages/routers/src/RoundRobin/roundRobin.ts
+++ packages/routers/src/RoundRobin/roundRobin.ts
@@ -1,28 +1,30 @@
 import { Endpoint, RouterApi } from '@scalecube/api';
 import { getFullAddress } from '@scalecube/utils';
 
+const lastEndPointMap: { [qualifier: string]: string } = {};
+
 export const roundRobin: RouterApi.Router = (options) => {
   const { message, lookUp } = options;
   const { qualifier } = message;
-  let lastEndPointIdentifier: any;
 
   const endpoints = lookUp({ qualifier });
   if (!(endpoints && Array.isArray(endpoints) && endpoints.length > 0)) {
     return null;
   } else {
-    if (!lastEndPointIdentifier) {
-      lastEndPointIdentifier = getFullAddress(endpoints[0].address);
+    if (!lastEndPointMap[qualifier]) {
+      lastEndPointMap[qualifier] = getFullAddress(endpoints[0].address);
       return endpoints[0];
     } else {
+      const lastEndPointIdentifier = lastEndPointMap[qualifier];
       const lastIndex = endpoints.findIndex((endpoint: Endpoint) => {
         return getFullAddress(endpoint.address) === lastEndPointIdentifier;
       });
 
-      if (lastIndex + 1 > endpoints.length) {
-        lastEndPointIdentifier = getFullAddress(endpoints[0].address);
+      if (lastIndex + 1 >= endpoints.length) {
+        lastEndPointMap[qualifier] = getFullAddress(endpoints[0].address);
         return endpoints[0];
       } else {
-        lastEndPointIdentifier = getFullAddress(endpoints[lastIndex + 1].address);
+        lastEndPointMap[qualifier] = getFullAddress(endpoints[lastIndex + 1].address);
         return endpoints[lastIndex + 1];
       }
     }
