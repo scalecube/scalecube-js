8f6743f6aed60cb17f4830b4cd46bf32559f08e6
idanilt <idan@ilt.co.il>
2020-05-31 17:37:05 +0300

    test release draft

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -26,20 +26,20 @@ if [[ "$BRANCH" =~ ^feature\/.*$ ]]; then
     else
         echo $MSG_PUBLISH_FAIL
     fi
-elif [[ "$BRANCH" == "develop" ]] && [[ "$IS_PULL_REQUEST" == "false" ]]; then
-    echo "--------------------------------------------"
-    echo "|     Deploying latest on npm registry     |"
-    echo "--------------------------------------------"
-
-    git remote set-url origin https://${GH_TOKEN}@github.com/scalecube/scalecube-js.git
-    git checkout develop
-    lerna publish prerelease --dist-tag next --preid next --yes -m '[skip ci]' --no-git-tag-version --no-push
-
-    if [[ "$?" == 0 ]]; then
-        echo $MSG_PUBLISH_SUCCESS
-    else
-        echo $MSG_PUBLISH_FAIL
-    fi
+#elif [[ "$BRANCH" == "develop" ]] && [[ "$IS_PULL_REQUEST" == "false" ]]; then
+#    echo "--------------------------------------------"
+#    echo "|     Deploying latest on npm registry     |"
+#    echo "--------------------------------------------"
+#
+#    git remote set-url origin https://${GH_TOKEN}@github.com/scalecube/scalecube-js.git
+#    git checkout develop
+#    lerna publish prerelease --dist-tag next --preid next --yes -m '[skip ci]' --no-git-tag-version --no-push
+#
+#    if [[ "$?" == 0 ]]; then
+#        echo $MSG_PUBLISH_SUCCESS
+#    else
+#        echo $MSG_PUBLISH_FAIL
+#    fi
 #elif [[ "$BRANCH" == "master" ]] && [[ "$IS_PULL_REQUEST" == "false" ]]; then
 #    echo "--------------------------------------------"
 #    echo "|     Deploying stable on npm registry     |"
