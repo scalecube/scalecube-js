eeb4efe4866f36ccdada69019010601d8a02f807
idanilt <idan@ilt.co.il>
2020-05-31 20:27:11 +0300

    publish develop preview version 3

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -34,7 +34,7 @@ elif [[ "$BRANCH" == "develop" ]] && [[ "$IS_PULL_REQUEST" == "false" ]]; then
     git remote set-url origin https://${GH_TOKEN}@github.com/scalecube/scalecube-js.git
     git checkout develop
     #yarn lerna publish --canary --dist-tag next --preid develop.$(date +%s) --yes
-    lerna publish prerelease --dist-tag next --preid develop.$(date +%s) --yes -m '[skip ci]' --no-git-tag-version --no-push
+    yarn lerna publish prerelease --dist-tag next --preid develop.$(date +%s) --yes -m '[skip ci]' --no-git-tag-version --no-push
 
     if [[ "$?" == 0 ]]; then
         echo $MSG_PUBLISH_SUCCESS
