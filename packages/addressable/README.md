4f250437e1e0645d6731ad3299cc880fddd72fb9
IdanILT <idan@ilt.co.il>
2020-07-28 20:05:52 +0300

    added tests fix readme

--- packages/addressable/README.md
+++ packages/addressable/README.md
@@ -1,23 +1,30 @@
 # Addressable
+
 A lightweight standalone micro lib to create addressable post messages.  
 Let say you have some post messages on the main thread a couple of IFrames and a few web workers.  
 Sending message between this element will be a nightmare!
 
 The solution, **addresses** like a plan tcp/ip network, or the WEB
 
-* This package is independent it maintained under scalecube-js only for convenience.
+-   This package is independent it maintained under scalecube-js only for convenience.
 
 # API
+
 Add thread to network; you must add it to main thread in order iframe/workers will work
+
 ```ts
 import '@scalecube/addessable';
 ```
+
 listen for messages on address
+
 ```ts
 import {listen} from '@scalecube/addessable';
 listen("address", (port: MessagePort)=>{port.postMessage("pong")});
 ```
+
 connect to address
+
 ```ts
 import {connect} from '@scalecube/addessable';
 const port = connect("address");
@@ -28,10 +35,8 @@ port.postMessage("ping");
 ```
 
 # How it's works
-- Each time you import addressable it will create global event listener in each thread
-- Threads will register them self by sending a message to main thread
-- Each time new listener added it will spread to main thread and to all thread from there
-- When connect is called it will create a message channel and give a port to listen callback and open side 
- 
 
-
+-   Each time you import addressable it will create global event listener in each thread
+-   Threads will register them self by sending a message to main thread
+-   Each time new listener added it will spread to main thread and to all thread from there
+-   When connect is called it will create a message channel and give a port to listen callback and open side 
--- packages/addressable/tests/connection.spec.ts
+++ packages/addressable/tests/connection.spec.ts
@@ -1,7 +1,7 @@
 import { createConnectionClient } from '../src/ConnectionClient';
 import { createConnectionServer } from '../src/ConnectionServer';
 
-describe('connection suite', () => {
+function createServerAndClients() {
   const ch1 = new MessageChannel();
   ch1.port1.start();
   ch1.port2.start();
@@ -18,8 +18,12 @@ describe('connection suite', () => {
 
   client1.createChannel(ch1.port2.postMessage.bind(ch1.port2));
   client2.createChannel(ch2.port2.postMessage.bind(ch2.port2));
+  return { client1, client2 };
+}
 
+describe('connection suite', () => {
   test('ping pong', async (done) => {
+    const { client1, client2 } = createServerAndClients();
     client1.listen('my address', (msg, p) => {
       msg.data === 'ping' && p.postMessage('pong');
     });
@@ -29,5 +33,40 @@ describe('connection suite', () => {
       done();
     });
     port.postMessage('ping');
+  });
+  test('connect before listen', (done) => {
+    const { client1, client2 } = createServerAndClients();
+    client1.connect('client2').then((port) => {
+      port.addEventListener('message', (e) => {
+        expect(e.data).toBe('pong');
+        done();
+      });
+      port.postMessage('ping');
+    });
+    setTimeout(() => {
+      client2.listen('client2', (msg, p) => {
+        msg.data === 'ping' && p.postMessage('pong');
+      });
+    }, 200);
+  });
+  test('Connection timeout', (done) => {
+    const { client1, client2 } = createServerAndClients();
+    client1
+      .connect('client2', 20)
+      .then((port) => {
+        port.addEventListener('message', () => {
+          expect(1).toBe(0);
+        });
+        port.postMessage('ping');
+      })
+      .catch((e) => {
+        expect(e).toBe('connection timeout');
+        done();
+      });
+    setTimeout(() => {
+      client2.listen('client2', (msg, p) => {
+        msg.data === 'ping' && p.postMessage('pong');
+      });
+    }, 200);
   });
 });
