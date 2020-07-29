d9212323714e25ffb62e0d91af7f9998ea37c739
IdanILT <idan@ilt.co.il>
2020-07-29 15:48:01 +0300

    fix start browser

--- packages/addressable/e2eStart.js
+++ packages/addressable/e2eStart.js
@@ -1,5 +1,7 @@
 const { exec } = require('child_process');
 
+const start = exec('yarn start', { cwd: '../../' });
+
 module.exports = {
-  start: exec('yarn start', { cwd: '../../' }),
+  start,
 };
